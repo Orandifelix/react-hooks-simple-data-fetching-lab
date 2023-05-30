d066b6d4edb357b0adb0baa4acdb757100fddb0c
Orandifelix <125347734+Orandifelix@users.noreply.github.com>
2023-05-30 11:11:26 +0300

    Finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
-// create your App component here
+
+import React, { useState, useEffect } from "react";
+
+export default function App() {
+  const [dogImage, setDogImage] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const url = "https://dog.ceo/api/breeds/image/random";
+
+  useEffect(() => {
+     setIsLoading(true);
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setDogImage(data.message);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  } else {
+    return <img src={dogImage} alt="A Random Dog" />;
+  }
+}
